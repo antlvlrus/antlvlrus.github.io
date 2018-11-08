f303ebd12a42605e7c9349894e1afd2cbf60982d
virusdetect <40795244+antlvlrus@users.noreply.github.com>
2018-11-08 20:45:12 +0300

    228
    228

---
+++ index.php
@@ -0,0 +1,235 @@
+ <!-- ==================================  kod trk ================================== -->
+ <?php
+// Query params are:  
+
+require_once dirname(__FILE__) . '/kclick_client.php';
+$client = new KClickClient('http://gro1n.com/api.php?', 'clmk58jw1dwrbdysvx98jncrnbnbpbtt');
+$client->sendAllParams();       // to send all params from page query
+$client->forceRedirectOffer();       // redirect to offer if an offer is chosen
+// $client->param('sub_id_5', '123'); // you can send any params
+// $client->keyword('PASTE_KEYWORD');  // send custom keyword
+// $client->currentPageAsReferrer(); // to send current page URL as click referrer
+// $client->debug();              // to enable debug mode and show the errors
+// $client->execute();             // request to api, show the output and continue
+$client->executeAndBreak();     // to stop page execution if there is redirect or some output
+?>
+ <!-- ==================================  kod trk ================================== -->
+
+<!DOCTYPE html>
+<html prefix="og: http://ogp.me/ns#" dir="ltr" lang="ru-RU">
+<head>
+<meta content="text/html; charset=UTF-8" http-equiv="Content-Type">
+<meta charset="UTF-8">
+<!-- change -->
+<title>Какое
+питание необходимо для здоровья человека</title>
+<!-- change -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+<link rel="apple-touch-icon" sizes="180x180" href="images/logo.gif">
+<link media="all" href="css/index.css" type="text/css" rel="stylesheet">
+</head>
+<body style="overflow-y: auto;">
+ <!-- change -->
+<meta property="og:description" content="Какое
+питание необходимо для здоровья человека" />
+ <!-- change -->
+
+<section class="header" style="display: block;">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="logo-img f_left"><a href=""><img src="images/logo.gif" alt="Evehealth" width="122" height="24"></a></div>
+                <nav class="main-mnu f_left">
+                    <ul>
+					 <!-- change -->
+                        <li><a href="">Новости<i class="mdi mdi-chevron-down f_right"></i></a></li>
+                        <li><a href="">В мире<i class="mdi mdi-chevron-down f_right"></i></a></li>
+                        <li><a href="">Рецепты<i class="mdi mdi-chevron-down f_right"></i></a></li>
+                        <li><a href="">Спорт<i class="mdi mdi-chevron-down f_right"></i></a></li>
+                        <li><a href="">Здоровье<i class="mdi mdi-chevron-down f_right"></i></a></li>
+                        <li><a href="">Магазины<i class="mdi mdi-chevron-down f_right"></i></a></li>
+                        <li class="promo-link"><a href="">Акции</a></li>
+                        <li class="search-link"><a href=""><i class="mdi mdi-magnify"></i></a></li>
+						<!-- change -->
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div><!-- ./container -->
+</section>
+    
+    <div id="bg"></div>
+
+<section class="article">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="main-img">
+				<!-- change -->
+                    <img src="images/banner.jpg" alt="ფიტნეს აერობიკის და ფიტნესის-სასწავლო">
+					<!-- change -->
+                    <div class="header-img">
+                        <div class="title-header">
+						<!-- change -->
+                            <h1>Какое
+питание необходимо для здоровья человека</h1>
+							<!-- change -->
+                        </div>
+                    </div>
+                </div>
+                <div class="text-frame">
+                    <div class="clearfix visible-xs-block"></div>
+                </div>
+                
+                <div id="toc_container" class="toc_white no_bullets">
+
+                    
+                    
+                    
+                    <!-- ==================================  контент ================================== -->
+ <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+  <meta content="text/html; charset=Windows-1251"
+ http-equiv="content-type">
+  <title></title>
+</head>
+<body>
+<h1 class="uk-article-title"
+ style="margin: 0px 0px 20px; font-family: &quot;Open Sans&quot;; font-weight: 300; line-height: 42px; color: rgb(68, 68, 68); font-size: 36px; text-transform: none; font-style: normal; letter-spacing: normal; orphans: 2; text-indent: 0px; white-space: normal; widows: 2; word-spacing: 0px; background-color: rgb(255, 255, 255); text-align: center;">Какое
+питание необходимо для здоровья человека</h1>
+<p class="uk-article-meta"
+ style="margin: 20px 0px; font-size: 12px; line-height: 18px; color: rgb(153, 153, 153); font-family: Arial,Helvetica,sans-serif; font-style: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; background-color: rgb(255, 255, 255);">Дата
+публикации<span>&nbsp;</span><time
+ datetime="2018-09-17">17 сентября 2018</time>.<span></span></p>
+<div
+ style="color: rgb(68, 68, 68); font-family: Arial,Helvetica,sans-serif; font-size: 16px; font-style: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; background-color: rgb(255, 255, 255);">
+<p style="margin: 0px 0px 20px;"><img
+ src="http://www.vitamarg.com/images/img1/pitanie-neobkhodimo-dlya-zdorovya-cheloveka.jpg"
+ alt="Какое питание необходимо для здоровья человека"
+ style="border: 0px none ; max-width: 100%; height: auto; vertical-align: middle;"></p>
+<p style="margin: 20px 0px;">Организму человека необходимо
+разнообразное питание ежедневно! Если употреблять часто что-то одно. То
+со временем продукт начнёт хуже усваиваться. Какие же продукты стоит
+есть, в чём придётся себя ограничить, чтобы чувствовать себя в
+прекрасной форме, на примере данной статьи.</p>
+<p style="margin: 20px 0px;"><em
+ style="font-style: italic;">Что учитывать</em></p>
+<h3
+ style="margin: 30px 0px 20px; font-family: &quot;Open Sans&quot;; font-weight: 300; line-height: 30px; color: rgb(51, 153, 102); font-size: 24px; text-transform: none;">1.
+Вода</h3>
+<p style="margin: 20px 0px;">Всем наверняка понятно, что
+выпивать надо по 1.5 &ndash; 2 литра в день. Мир успел придумать
+даже некоторые формулы подходящие для мужчин и женщин. Ещё важно
+соблюдать питьевой режим, если человек активно занимается спортом.</p>
+<h3
+ style="margin: 30px 0px 20px; font-family: &quot;Open Sans&quot;; font-weight: 300; line-height: 30px; color: rgb(51, 153, 102); font-size: 24px; text-transform: none;">2.
+Микро, макро</h3>
+<p style="margin: 20px 0px;">Эти элементы необходимы
+организму. Проще всего просто включить в рацион употребление различных
+овощей, фруктов. А также необходимо нормализовать углеводы и клетчатку.</p>
+<h3
+ style="margin: 30px 0px 20px; font-family: &quot;Open Sans&quot;; font-weight: 300; line-height: 30px; color: rgb(51, 153, 102); font-size: 24px; text-transform: none;">3.
+Количество еды</h3>
+<p style="margin: 20px 0px;">В среднем необходимо питаться
+3-4 раза в день. Людям, имеющим заболевания желудочно-кишечного тракта,
+врачи часто рекомендуют диеты и питание до 5 раз в день. Если очень
+сильно захотелось перекусить, то это можно сделать с помощью фруктов.</p>
+<h3
+ style="margin: 30px 0px 20px; font-family: &quot;Open Sans&quot;; font-weight: 300; line-height: 30px; color: rgb(51, 153, 102); font-size: 24px; text-transform: none;">4.
+Какая еда полезна</h3>
+<p style="margin: 20px 0px;">Нужно есть только свежие
+продукты. Полуфабрикаты, пиццы стоит исключить или свести к минимуму.</p>
+<p style="margin: 20px 0px;">Даже если человек начнёт всё
+это делать, насколько же его хватит? День или неделю? Тем временем
+чтобы от этого питания был толк, нужно намного больше времени. Но
+взамен любой получит &ndash; нормально функционирующий организм.
+Если вдруг возникли вопросы к своему рациону. Желательно составить
+список на бумаге или компьютере, чем питается человек. Вскоре он сможет
+трезво оценить группы продуктов и их питательные вещества. Далее можно
+составить список под себя. Также можете посетить&nbsp;<span
+ style="background: transparent none repeat scroll 0% 50%; -moz-background-clip: initial; -moz-background-origin: initial; -moz-background-inline-policy: initial; color: rgb(31, 162, 214); text-decoration: none; cursor: pointer;">курсы
+кондитера</span>, чтобы лучше разбираться в сочетании продуктов.</p>
+<h3
+ style="margin: 30px 0px 20px; font-family: &quot;Open Sans&quot;; font-weight: 300; line-height: 30px; color: rgb(51, 153, 102); font-size: 24px; text-transform: none;">Калорийность
+= энергия</h3>
+<p style="margin: 20px 0px;">Энергия это очень ценный
+ресурс. Он помогает людям двигаться и заниматься спортом, активно
+проявлять себя на работе и дома, наращивать мышцы и сохранять
+иммунитет. На данный момент существуют десятки различных приложений,
+которые с огромной радостью посчитают калории человека и дадут
+оптимальные советы. В интернете есть специальные формулы под каждое
+блюдо, но это совсем уже для отчаянных.</p>
+<p style="margin: 20px 0px;">Метод здоровой тарелки.
+Потребуется пустая тарелка, которую необходимо мысленно разделить на 4
+части. Две половины сразу занимают овощи, а в другую, по четвертям,
+кладётся белковая пища и крупы. Пару замечаний. Картофель кладётся к
+овощам, использоваться жирные соусы нельзя, в тарелке можно
+использовать нежирное мясо, птицу и яйца. Важно помнить, что энергия
+измениться, так что надо аккуратно расходовать её. Если человек будет
+чувствовать, что сил нет, лучше отдохнуть. Вскоре организм привыкнет и
+всё нормализуется. Хотя подобного обычно не случается.</p>
+<h3
+ style="margin: 30px 0px 20px; font-family: &quot;Open Sans&quot;; font-weight: 300; line-height: 30px; color: rgb(51, 153, 102); font-size: 24px; text-transform: none;">Подарки
+от организма</h3>
+<p style="margin: 20px 0px;">Вскоре организм будет
+радовать человека. Сердцебиение начнёт улучшаться, уберётся лишний вес,
+как было отмечено выше энергия нормализуется и начнёт расти. Ещё
+произойдёт снижение уровня холестерина, улучшится здоровье кожи. Кожа
+будет лучше выводить токсины и возможные проблемы с акне будут меньше.
+Будет улучшена работа ЖКТ и иммунной системы, человек будет более
+защищён от стрессов. Если ко всему этому добавить умеренные ежедневные
+тренировки, то тело вскоре сможет приобрести подтянутую и стройную
+форму.&nbsp;</p>
+<p style="margin: 20px 0px;">Ещё важно понимать, что если
+человеку не достаточно еды, ему стоит питаться чаще. Ведь недостаточное
+употребление пищи, ведёт к снижению не только энергии, но и умственно и
+физической деятельности. Тогда о результатах можно забыть. Жирную,
+копчёную пищу и сладости, надо свести к минимальному употреблению,
+молочную продукцию, мясо, рыба, яйца, орехи и овощи с фруктами можно
+есть чаще. Важно в рационе также иметь каши, хлеб, рис и другие, цельно
+зерновые продукты. Сначала будет тяжело, но сколько человек отдаёт,
+столько он и получит.</p>
+</div>
+</body>
+</html>
+
+
+                    <!-- ==================================  контент ================================== -->
+                     
+                    
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    </section>
+	<br>
+<div style="text-align: center;"><span
+ style="font-weight: bold;">____________________________</span><span
+ style="font-weight: bold;"></span><br>
+</div>
+<div style="text-align: center;">Использование
+фотоматериалов разрешено только с письменного согласия администрации
+сайта. <br>
+Размещение объектов интеллектуальной собственности (фото, видео,
+литературные произведения, товарные знаки и т.д.) <br>
+на сайте разрешено только лицам, имеющим все необходимые права для
+такого размещения. <br>
+Copyright (с) 2016-2018 <br>
+</div>
+<br>
+<div style="text-align: center;"><a
+ style="text-decoration: underline;" href="privacypolicy.htm">Политика
+конфиденциальности</a><br>
+</div>
+<br>
+
+    
+    <div class="btntop"></div>
+    
+    <link rel="stylesheet" href="css/css.css">
+
+</body>
+</html>
